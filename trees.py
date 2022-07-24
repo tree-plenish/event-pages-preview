740b02650d74eb1b7c9db206276c1b83b3d2d642
cwang360 <cntawng24@gmail.com>
2022-07-24 01:13:54 -0400

    Autofill for hosts and trees

---
+++ trees.py
@@ -0,0 +1,6 @@
+tree_photos = { # for testing
+    'Birch' : 'https://drive.google.com/uc?export=view&id=1vsARAiypRQtf_oRllBLCwTflPJGgoB1p',
+    'Maple' : 'https://drive.google.com/uc?export=view&id=1GEywvibdvOlwlKK84K1HMQk0yOl4VCUV',
+    'Redwood' : 'https://drive.google.com/uc?export=view&id=1n7dqlPIfisjX7hDtP4m7rFwSQB8d3HTA',
+    'Spruce' : 'https://drive.google.com/uc?export=view&id=1sLx7FA9LFFem0zT9GdMGyjEqp0vMNLKP',
+}
